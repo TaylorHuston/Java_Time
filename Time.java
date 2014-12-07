04bff0875d089d171c1ea65689db8c9c94719bb9
Taylor Huston <taylor.a.huston@gmail.com>
2014-12-07 03:05:57 -0700

    Add description

--- Time.java
+++ Time.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Simple program to demonstray basic exception handling
+ * From "Java How To Program, 10/e, Early Objects" - Chapter 8.
  */
 
 public class Time {
